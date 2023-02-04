a74481f461942ac46221f78c4bcc5084490f406e
Rodrick <jesprikovic@gmail.com>
2023-02-04 12:16:06 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CV Application
+
+Created using Vite, React, Tailwind and Scss.
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+    // Event function binders here
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>CV Application</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
